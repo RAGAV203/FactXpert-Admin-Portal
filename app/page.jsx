64e5165cb93f322c769f0f44502b06be044f9310
RAGAV <121377164+RAGAV203@users.noreply.github.com>
2024-09-20 14:25:42 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -19,31 +19,17 @@ const Homepage = () => {
         <center>
           <div className={styles.newbtn}>
             <Link href={`/login`}>
-              <button className={`${styles.btn}`}>Login</button>
+              <button className={`${styles.btn}`}>Admin Login</button>
             </Link>
-          </div>
-        </center>
-      </div>
-      &nbsp;&nbsp;&nbsp;
-      <div className={styles.textContainer}>
-      <center><h1 className={styles.title}>
-      <SiExpertsexchange /></h1>
-    </center>
-        <h1 className={styles.title}>FactXpert</h1><h2>Admin Portal </h2>
-
-        
-        <p className={styles.desc}>
-          Welcome <br />Login to Continue
-        </p>
-       
-        <center>
+          </div> &nbsp;&nbsp;&nbsp;
           <div className={styles.newbtn}>
-            <Link href={`/login`}>
-              <button className={`${styles.btn}`}>Login</button>
+            <Link href={`https://fact-xpert-quiz-portal.vercel.app/login`}>
+              <button className={`${styles.btn}`}>User Login</button>
             </Link>
           </div>
         </center>
       </div>
+      
     </div>
   );
 };
